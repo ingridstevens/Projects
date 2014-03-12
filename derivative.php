a6a04490632f1c0f3dd6ad1e747cf5958cf26ced
Ingrid Stevens <icklebobz@gmail.com>
2014-03-11 17:34:59 -0700

    added b=0 special case

--- derivative.php
+++ derivative.php
@@ -1,7 +1,5 @@
-<script type="text/javascript" src="functions.js"></script>
-
 <head>
-<title>Calculations and Playing with JavaScript</title>
+<title>PHP Derivative Calculator</title>
 </head>
 
 <h2>Do some derivatives :)</h2>
@@ -17,14 +15,20 @@ a<i>x</i><sup>b</sup>
 <?php
 
 function derivative($a, $b) {
-
+	global $comments;
 	if( isset( $_POST["submit"] ) ) {
-		$coefficient = $a * $b;
-		$power = $b - 1;
-		$answer = $coefficient . "<i>x</i><sup>" . $power . "</sup>" ;
-		echo $answer;
+		if($b == 0) {
+			$answer = "0";
+			$comments = "Because the value you but in for b was 0, the line is horizontal, which means that the derivative (or slope) is 0.";
+		}else {
+			$coefficient = $a * $b;
+			$power = $b - 1;
+			$answer = $coefficient . "<i>x</i><sup>" . $power . "</sup>" ;
+			$comments = "";
+		}
+		return $answer;
 	}
-	
+
 }
 
 $a = $_POST['inputa'];
@@ -37,14 +41,15 @@ $original = "f(x) = $a<i>x</i><sup>$b</sup>";
 if (is_numeric($a) && is_numeric($b)) {
 	echo "Your original function was <b>$original</b><br>";
 	echo "The derivative of $original calculated using the power rule, is <b>";
-	derivative( $a, $b );
+	echo derivative( $a, $b );
+	echo "</b><p>";
+	echo $comments;
 
 
-	echo "</b><br><br>Learn more about <a href='https://www.khanacademy.org/math/calculus/differential-calculus/power_rule_tutorial/v/power-rule'>power rule.</a>";
+	echo "</p><br><br>Learn more about <a href='https://www.khanacademy.org/math/calculus/differential-calculus/power_rule_tutorial/v/power-rule'>power rule.</a>";
 
 }else {
 	echo "Please enter some numeric values";
 }
 
 ?>
-
